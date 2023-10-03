a195de79bd86bcc759d93566b2901cc782f49470
unknown <harianja.suryo@gmail.com>
2023-10-03 07:58:41 +0700

    update key

--- allFunctions.php
+++ allFunctions.php
@@ -17,7 +17,7 @@ function connectMailChimp() {
 	$client = new \MailchimpMarketing\ApiClient();
 
 	$client->setConfig([
-		'apiKey' => 'd981dd68c9abb58aa8cb0e07f1740be5-us14',
+		'apiKey' => '99c04494bdee35d5fe41e10cb0f6cd63-us14',
 		'server' => 'us14'
 	]);
 
--- submitSubscribe.php
+++ submitSubscribe.php
@@ -5,7 +5,7 @@ require_once('vendor/autoload.php');
 $client = new \MailchimpMarketing\ApiClient();
 
 $client->setConfig([
-	'apiKey' => 'd981dd68c9abb58aa8cb0e07f1740be5-us14',
+	'apiKey' => '6d3f826edadbc02a833f8023d560b7c4-us14',
 	'server' => 'us14'
 ]);
 
